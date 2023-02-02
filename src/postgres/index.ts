54b0ab73a1a9c845b876efa7474e57647b11b8e2
Onwordi Prince Alfred <princelini4@gmail.com>
2023-02-02 16:17:25 +0100

    updated pg connection

--- src/postgres/index.ts
+++ src/postgres/index.ts
@@ -1,17 +1,20 @@
-import {Pool} from "pg";
+import { Pool } from "pg";
 
-const {DB_PORT,
-    DB_NAME,
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD} = process.env;
+const { DB_PORT, DB_NAME, DB_HOST, DB_USER, DB_PASSWORD, DB_URI, NODE_ENV } =
+  process.env;
 
 const pool = new Pool({
-    host: DB_HOST,
-    user: DB_USER,
-    database: DB_NAME,
-    port: Number(DB_PORT),
-    password: DB_PASSWORD,
-  });
+  ...(NODE_ENV === "development"
+    ? {
+        host: DB_HOST,
+        user: DB_USER,
+        database: DB_NAME,
+        port: Number(DB_PORT),
+        password: DB_PASSWORD,
+      }
+    : {
+        connectionString: DB_URI,
+      }),
+});
 
 export default pool;
